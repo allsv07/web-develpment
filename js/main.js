81c589d002bbe797366d6705e07e27182b324b36
Your Name <test@example.com>
2021-02-09 22:08:24 +0300

    layout

---
+++ js/main.js
@@ -0,0 +1,17 @@
+//down
+$(document).ready(function(){
+    const anchors = document.querySelectorAll('a[href*="#"]');
+    for (let anchor of anchors) {
+        anchor.addEventListener("click", function(event){
+            event.preventDefault();
+            const blockID = anchor.getAttribute('href');
+            document.querySelector('' + blockID).scrollIntoView({
+                behavior: "smooth",
+                block: "start"
+            });
+        });
+    }
+});
+
+//headhesive
+var header = new Headhesive('.top-menu-block');
